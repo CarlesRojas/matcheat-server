b7bf351f41c16f5b28213ad1dad98bd842fcb2ea
Carles <carles.gstocks@gmail.com>
2021-02-10 18:19:29 +0100

    Username limit

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         min: 5,
-        max: 32,
+        max: 16,
     },
     email: {
         type: String,
